df27504d0b53e15006a491fc29dc866206b18977
Umberban <romanovborysv@gmail.com>
2023-03-05 01:02:57 +0200

    1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,52 @@
 import css from "../modules/App.module.css"
-export const App = () => {
+import PropTypes from "prop-types";
+import user from "../user.json"
+const Description = ({ avatar, username,location,tag }) => (
+  <div className={css.description}>
+    <img
+      src={avatar}
+      alt="User avatar"
+      className={css.avatar}
+    />
+    <p className={css.username}>{username}</p>
+    <p className={css.tag}>{tag}</p>
+    <p className={css.location}>{location}</p>
+  </div>
+);
+const Stat = ({stats,quantity,type})=>(
+  <li className={css.stat} >
+    <span className={css.label}>{type}</span>
+    <span className={css.quantity}>{stats[quantity]}</span>
+  </li>
+)
+
+const Stats = ()=> (
+<ul className={css.stats}>
+  <Stat stats={user.stats} type="Followers" quantity="followers" ></Stat>
+  <Stat stats={user.stats} type="Likes" quantity="likes" ></Stat>
+  <Stat stats={user.stats} type="Views" quantity="views" ></Stat>
+</ul>
+
+);
+export const App = (children) => {
   return (
-    <div
-      className={css.app}
-    >
-      React homework template
-    </div>
+    <div className={css.app}>
+      <Description username={user.username}
+         tag={user.tag}
+          location={user.location}
+          avatar={user.avatar} />
+      <Stats stats={user.stats}/>
+  
+</div>
   );
+};
+Description.propTypes = {
+  username:PropTypes.string,
+  tag:PropTypes.string,
+  location:PropTypes.string,
+  avatar:PropTypes.string,
+};
+Stat.propTypes = {
+  stats:PropTypes.object,
+  
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-
+import user from "./user.json"
+import PropTypes from "prop-types";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
+console.log(user);
