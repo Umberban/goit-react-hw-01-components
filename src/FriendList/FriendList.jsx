6bc68e3a8aff29b7e12cd1c5f315c232e2b99ab4
Umberban <romanovborysv@gmail.com>
2023-03-06 00:20:00 +0200

    Update FriendList.jsx

--- src/FriendList/FriendList.jsx
+++ src/FriendList/FriendList.jsx
@@ -5,10 +5,10 @@ import data from "./friends.json";
 
 export const FriendList = ({array})=>{
     return( <ul className="friend-list">
-    {array.map((item)=> <li key={item.id} className={css.item}>
-        <img srs={item.avatar} alt="profile-avatar" className={css.image}/>
+    {array.map(item => (<li key={item.id} className={css.item}>
+        <img src={item.avatar} alt="profile-avatar" className={css.image}/>
         <p className={css.name}>{item.percentage}</p>
-      </li> )}
+      </li> ))}
   </ul>
 )
 
