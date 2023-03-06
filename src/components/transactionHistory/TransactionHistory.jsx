e32e1843bf705b2dffea0934eed67892dad58ae8
Umberban <romanovborysv@gmail.com>
2023-03-06 16:44:34 +0200

    finished

---
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -0,0 +1,24 @@
+import css from "./TransactionHistory.module.css";
+import PropTypes from "prop-types";
+
+export const TransactionHistory = ({data})=>{
+    return(
+    <table className={css.transactionTable}>
+  <thead className={css.transactionTable__head}>
+    <tr>
+      <th className={css.transactionTable__th}>Type</th>
+      <th className={css.transactionTable__th}>Amount</th>
+      <th className={css.transactionTable__th}>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+  {data.map(item=>(
+    <tr key={item.id} className={css.transactionTable__tr}>
+      <td className={css.transactionTable__td}>{item.type}</td>
+      <td className={css.transactionTable__td}>{item.amount}</td>
+      <td className={css.transactionTable__td}>{item.currency}</td>
+    </tr>))}
+  </tbody>
+</table>)
+}
--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import './index.css';
 import data from "./components/statist/data.json";
 import friendList from "./components/friendList/friends.json";
 import user from "./components/socialCard/user.json";
+import transactions from "./components/transactionHistory/transactions.json"
+import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Profile data={user}/>
     <Statistics array={data} title="Upload stats" />
     <FriendList array = {friendList}/>
+    <TransactionHistory data = {transactions}/>
   </React.StrictMode>
 );
 console.log(data);
